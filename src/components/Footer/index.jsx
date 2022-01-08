02ebdbd8bf9bc216fea8f6325287af5264428e70
duongGithub123 <duongngo7497@gmail.com>
2022-01-08 15:22:30 +0700

    Create Footer Component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,79 @@
+import './index.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <div className='footer_contact'>
+                <div className='title'>TECHISM</div>
+                <div className='summary'>
+                    We are always seeking people who share our values and take pride in their craft.
+                    We strive to inspire you to be the best you can be, as well as give you the benefits,
+                    job security, and growth opportunities you need.
+                </div>
+                <div className='Address'>
+                    <strong>Địa chỉ văn phòng: </strong>
+                    113/2 Hoàng Văn Thái, P.Hòa Khánh Nam, Q.Liên Chiểu, Tp.Đà Nẵng
+                </div>
+                <div className='phone'>
+                    <strong>Điện thoại: </strong>
+                    076 541 1541
+                </div>
+                <div className='email'>
+                    <strong>Mail: </strong>
+                    techism.vn@gmail.com
+                </div>
+            </div>
+            <div className='footer_menu'>
+                <div className='title'>Danh mục</div>
+                <div className='main_menu'>Trang chủ</div>
+                <div className='main_menu'>Giới thiệu</div>
+                <div className='main_menu'>Dịch vụ</div>
+                <div className='main_menu'>Mẫu giao diện</div>
+                <div className='main_menu'>Tin tức</div>
+                <div className='main_menu'>Liên hệ</div>
+            </div>
+            <div className='footer_media_contact'>
+                <div className='title'>TECHISM</div>
+                <div className='summary'>
+                    We are always seeking people who share our values and take pride in their craft.
+                    We strive to inspire you to be the best you can be, as well as give you the benefits,
+                    job security, and growth opportunities you need.
+                </div>
+                <div className='Address'>
+                    <strong>Địa chỉ văn phòng: </strong>
+                    113/2 Hoàng Văn Thái, P.Hòa Khánh Nam, Q.Liên Chiểu, Tp.Đà Nẵng
+                </div>
+                <div className='phone'>
+                    <strong>Điện thoại: </strong>
+                    076 541 1541
+                </div>
+                <div className='email'>
+                    <strong>Mail: </strong>
+                    techism.vn@gmail.com
+                </div>
+            </div>
+            <div className='contact'>
+                <div className='name'>TECHISM</div>
+                <div className='summary'>
+                    We are always seeking people who share our values and take pride in their craft.
+                    We strive to inspire you to be the best you can be, as well as give you the benefits,
+                    job security, and growth opportunities you need.
+                </div>
+                <div className='Address'>
+                    <strong>Địa chỉ văn phòng: </strong>
+                    113/2 Hoàng Văn Thái, P.Hòa Khánh Nam, Q.Liên Chiểu, Tp.Đà Nẵng
+                </div>
+                <div className='phone'>
+                    <strong>Điện thoại: </strong>
+                    076 541 1541
+                </div>
+                <div className='email'>
+                    <strong>Mail: </strong>
+                    techism.vn@gmail.com
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
