6b4a12b8256fd17737ea6332dfa0483b201cecc4
TienLe <leviettien1808@gmail.com>
2022-01-24 09:28:29 +0700

    Add Container component to wrap.

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,1 +1,6 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: ${(props) => props.backgroundColor || 'white'};
+  padding: 0px ${(props) => props.paddingHorizontal || '10%'};
+`;
