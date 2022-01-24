0a7350735b838fac223924ec414e843716af3e9c
TienLe <leviettien1808@gmail.com>
2022-01-24 09:27:14 +0700

    Add some color constants.

--- src/commons/constants.js
+++ src/commons/constants.js
@@ -1,3 +1,8 @@
 export const COMPANY_NAME = 'techism';
 export const DOMAIN_EXTENSION = '.vn';
-export const EMAIL = 'techism.vn@gmail.com'
+export const EMAIL = 'techism.vn@gmail.com';
+
+// Colors
+export const BACKGROUND_COLOR = '#2C2C2C';
+export const SELECTED_TEXT_COLOR = '#FFF';
+export const UNSELECTED_TEXT_COLOR = '#D8D8D8';
