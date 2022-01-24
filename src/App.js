49cf90ccfb90f97e40603859ba8b5967e83ebac8
TienLe <leviettien1808@gmail.com>
2022-01-24 09:37:39 +0700

    Update App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Footer from './components/Footer';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
-      <Footer/>
+      <Home />
     </div>
   );
 }
