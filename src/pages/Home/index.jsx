063b5fb00c9767b7f529c93a82e7191de51a6ec0
duongGithub123 <duongngo7497@gmail.com>
2022-01-30 22:48:01 +0700

    Update Responsive for Footer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import {use} from 'react';
-import { BACKGROUND_COLOR, PADDING_FOOTER } from '../../commons/constants';
+import * as constants from '../../commons/constants';
 import Footer from '../../components/Footer';
 import Navigation from '../../components/Navigation';
 import Copyright from '../../components/Copyright';
+import CustomerComment from '../../components/CustomerComment';
 import { Container } from '../../components/styles';
 import useWindowDimensions from '../../commons/useWindowDimensions';
+import CommentBg from '../../assets/images/comment_bg.svg';
 
 const Home = () => {
   const {width, height} = useWindowDimensions();
+  const paddingByWidth = width >= 1440 ? (width - 1440)/2 : 0;
   return (
     <>
-      <Container backgroundColor={BACKGROUND_COLOR}>
+      <Container backgroundColor={constants.BACKGROUND_COLOR}>
         <Navigation />
       </Container>
-      <Container height='327' paddingVertical='32' paddingHorizontal={(width >= 1440 && width <= 1920) ? (width - 1440)/2 + PADDING_FOOTER + 'px' : false}>
+      <Container backgroundImage={CommentBg} height='580' paddingVertical='102' paddingHorizontal={paddingByWidth + constants.PADDING_CUS_COMMENT+ 'px'}>
+        <CustomerComment />
+      </Container>
+      <Container paddingVertical='32' paddingHorizontal={paddingByWidth + constants.PADDING_FOOTER + 'px'}>
         <Footer />
       </Container>
-      <Container backgroundColor={BACKGROUND_COLOR} height='33' paddingVertical='3' paddingHorizontal={(width >= 1440 && width <= 1920) ? (width - 1440)/2 + PADDING_FOOTER + 'px' : false}>
+      <Container backgroundColor={constants.BACKGROUND_COLOR} height='33' paddingVertical='3' paddingHorizontal={paddingByWidth + constants.PADDING_FOOTER+ 'px'}>
         <Copyright />
       </Container>
     </>
