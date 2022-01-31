851b2ce567126ed1cb33ddf3fc0c117a975be5f6
TienLe <leviettien1808@gmail.com>
2022-01-31 20:43:25 +0700

    Add to Home.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,25 +8,50 @@ import SampleProduct from '../../components/SampleProduct';
 import { Container } from '../../components/styles';
 import useWindowDimensions from '../../commons/useWindowDimensions';
 import CommentBg from '../../assets/images/comment_bg.svg';
+import Slide from '../../components/Slide';
+import { BACKGROUND_COLOR } from '../../commons/constants';
 
 const Home = () => {
-  const {width, height} = useWindowDimensions();
-  const paddingByWidth = width >= 1440 ? (width - 1440)/2 : 0;
+  const { width, height } = useWindowDimensions();
+  const paddingByWidth = width >= 1440 ? (width - 1440) / 2 : 0;
   return (
     <>
       <Container backgroundColor={constants.BACKGROUND_COLOR}>
         <Navigation />
       </Container>
-      <Container paddingVertical='75' paddingHorizontal={paddingByWidth + constants.PADDING_SAMPLE_PRODUCT + 'px'}>
+      <Container backgroundColor={BACKGROUND_COLOR} fluid>
+        <Slide />
+      </Container>
+      <Container
+        paddingVertical="75"
+        paddingHorizontal={
+          paddingByWidth + constants.PADDING_SAMPLE_PRODUCT + 'px'
+        }
+      >
         <SampleProduct />
       </Container>
-      <Container backgroundImage={CommentBg} height='580' paddingVertical='102' paddingHorizontal={paddingByWidth + constants.PADDING_CUS_COMMENT+ 'px'}>
+      <Container
+        backgroundImage={CommentBg}
+        height="580"
+        paddingVertical="102"
+        paddingHorizontal={
+          paddingByWidth + constants.PADDING_CUS_COMMENT + 'px'
+        }
+      >
         <CustomerComment />
       </Container>
-      <Container paddingVertical='32' paddingHorizontal={paddingByWidth + constants.PADDING_FOOTER + 'px'}>
+      <Container
+        paddingVertical="32"
+        paddingHorizontal={paddingByWidth + constants.PADDING_FOOTER + 'px'}
+      >
         <Footer />
       </Container>
-      <Container backgroundColor={constants.BACKGROUND_COLOR} height='33' paddingVertical='3' paddingHorizontal={paddingByWidth + constants.PADDING_FOOTER+ 'px'}>
+      <Container
+        backgroundColor={constants.BACKGROUND_COLOR}
+        height="33"
+        paddingVertical="3"
+        paddingHorizontal={paddingByWidth + constants.PADDING_FOOTER + 'px'}
+      >
         <Copyright />
       </Container>
     </>
